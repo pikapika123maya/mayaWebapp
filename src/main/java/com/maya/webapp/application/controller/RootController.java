54cbb528ac2325e991e24a74a946061b80ba1f80
Thomas <hongkanliu@gmail.com>
2018-09-09 13:01:02 -0700

    add bean config

--- src/main/java/com/maya/webapp/application/controller/RootController.java
+++ src/main/java/com/maya/webapp/application/controller/RootController.java
@@ -1,16 +1,23 @@
 package com.maya.webapp.application.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RootController {
+    @Autowired
+    String biuString;
+
     @GetMapping("/hello")
-    public String hello(@RequestParam(name="name", required=false, defaultValue="world") String name, Model model) {
-        model.addAttribute("name", name);
-        return "hello"; // returns a view call "hello"
+    public ModelAndView hello(@RequestParam(name="name", required=false, defaultValue="world") String name) {
+        ModelAndView mav = new ModelAndView("hello");
+
+        mav.addObject("name", biuString);
+        System.out.println("in controller");
+        return mav; // returns a view call "hello"
     }
 
     @GetMapping("/")
---
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>HellO Wo</h2>
+</body>
+</html>
